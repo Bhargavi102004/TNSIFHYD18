7a256bfb0382d42d25e175607a07ab0eaa41a0a9
Bhargavi102004 <168522271+Bhargavi102004@users.noreply.github.com>
2024-05-03 20:05:32 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,17 @@
+package com.si.simple;
+
+public class Student {
+	int a=10;
+	static int b=20;
+
+	public static void main(String[] args) {
+		int c=30;
+		System.out.println(c);
+		Student s1=new Student();
+		System.out.println(s1.a);
+		System.out.println(b);
+		// TODO Auto-generated method stub
+
+	}
+
+}
